4764f066999efdc4fa89688ad50114f056957f65
Alexander Ratkevich <alexandr.ratkevich.1337@gmail.com>
2024-11-02 23:47:53 +0100

    fixes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,33 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+import {lusitana} from '@/app/ui/fonts';
+import {InvoicesTableSkeleton} from '@/app/ui/skeletons';
+import {Suspense} from 'react';
+
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+
+  return (
+      <div className="w-full">
+        <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+          Customers
+        </h1>
+        <Search placeholder="Search customers..."/>
+        <Suspense key={query} fallback={<InvoicesTableSkeleton/>}>
+          <Table query={query}/>
+        </Suspense>
+      </div>
+  );
 }
